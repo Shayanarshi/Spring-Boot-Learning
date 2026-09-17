0d9135103aef684f9ab31dacb2415aac443b7545
Shayanarshi <arshishayan@gmail.com>
2026-09-17 10:42:08 +0530

    Update sb-jpas-crud-demo

--- sb-jpas-crud-demo/src/main/java/in/ashokit/app/runner/MyAppRunner.java
+++ sb-jpas-crud-demo/src/main/java/in/ashokit/app/runner/MyAppRunner.java
@@ -8,6 +8,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class MyAppRunner implements ApplicationRunner {
 
@@ -17,17 +19,44 @@ public class MyAppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+//        saveEmployee();
+//         updateEmployee();
 
-        saveEmployee();
-//        updateEmployee();
+      /*  Employee e = fetchEmployeeById(7788L);
+        if(e !=null){
+            System.out.println("empno : " +e.getId());
+            System.out.println("Name  : " +e.getName());
+            System.out.println("Sal   : " +e.getSal());
+            System.out.println("Dept  : " +e.getDepartment());
+        }*/
+
+        deleteEmployee(7201L);
 
     }
+    private  void deleteEmployee(Long id){
+        repository.deleteById(id);
+    }
+
     private  void saveEmployee(){
         Employee employee = new Employee();
-        employee.setId(7101L);
-        employee.setName("John");
+        employee.setId(7943L);
+        employee.setName("EMMA");
         employee.setSal(7000.0);
-        employee.setDepartment("Accounting");
+        employee.setDepartment("HR");
         repository.save(employee);
+    }
+    private  void updateEmployee(){
+
+        Employee employee = new Employee();
+        employee.setId(7943L);
+        employee.setName("MARY");
+        employee.setSal(4000.0);
+        employee.setDepartment("HR");
+        repository.save(employee);
+    }
+
+    private Employee fetchEmployeeById(Long id){
+        Optional<Employee>opt =repository.findById(id);
+        return opt.orElse(null);
     }
 }
