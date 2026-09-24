0e345b3fd5b790dfa661052ea5445fcb7b39f222
Shayanarshi <arshishayan@gmail.com>
2026-09-24 16:13:51 +0530

    Added sb-jpa-query-2

--- sb-jpa-query-2/src/main/java/in/ashokit/repository/OrderRepository.java
+++ sb-jpa-query-2/src/main/java/in/ashokit/repository/OrderRepository.java
@@ -4,18 +4,41 @@ import in.ashokit.model.Order;
 import in.ashokit.model.OrderStatus;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
-    // find order by status
-    List<Order> findByStatus(OrderStatus status);
 
-    // find the orders by amount greater than
-    List<Order> findByAmountGreaterThan(Double amount);
+    //select the orders between two dates
+
+    @Query(value = "SELECT o FROM Order o WHERE o.orderDate BETWEEN ?1 AND ?2",nativeQuery = false)
+    List<Order> fetchOrdersBetweenDates(LocalDate startDate,LocalDate endDate);
+
+
+    // select the orders with status1 or status2 delivered or cancelled
+
+
+  /*@Query(value = "SELECT * FROM ORDERS WHERE STATUS = ? OR STATUS=? ",nativeQuery = true)
+    List<Order> getOrdersWithStatus(OrderStatus status1,OrderStatus status2);*/
+
+    @Query(value = "SELECT * FROM ORDERS WHERE STATUS = ? OR STATUS=? ",nativeQuery = true)
+    List<Order> getOrdersWithStatus(String status1,String status2);
+
+    @Query(value = "SELECT * FROM ORDERS WHERE Amount = (SELECT MAX(AMOUNT) FROM ORDERS)" ,nativeQuery = true)
+    List<Order> fetchHighestAmountOrder();
+
+    @Query(value="select o from Order o where o.amount > :amount and o.status = :sts")
+    List<Order>  fetchOrdersByStatus(@Param("amount")Double amount,
+                                                @Param("sts") OrderStatus status);
+
+
+
 
 
 }
--- sb-jpa-query-2/src/main/java/in/ashokit/runner/MyApplicationRunner.java
+++ sb-jpa-query-2/src/main/java/in/ashokit/runner/MyApplicationRunner.java
@@ -1,4 +1,54 @@
 package in.ashokit.runner;
 
-public class MyApplicationRunner {
+import in.ashokit.model.Order;
+import in.ashokit.model.OrderStatus;
+import in.ashokit.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class MyApplicationRunner implements ApplicationRunner {
+
+    @Autowired
+    OrderRepository repo;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+//        getOrdersBetweenDates();
+//        readTheOrderWithStatus();
+//        fetchTheHighestOrder();
+        fetchOrdersByStatus();
+    }
+
+    private void  fetchOrdersByStatus(){
+        List<Order> orderDetails = repo.fetchOrdersByStatus(1000.0,OrderStatus.DELIVERED);
+        orderDetails.forEach(System.out::println);
+
+    }
+    private void fetchTheHighestOrder(){
+        List<Order> orderList=repo.fetchHighestAmountOrder();
+        orderList.forEach(System.out::println);
+    }
+
+
+    private  void  readTheOrderWithStatus(){
+        List<Order> ordersWithStatus=repo.getOrdersWithStatus(OrderStatus.PLACED.name(),OrderStatus.CANCELLED.name());
+        ordersWithStatus.forEach(System.out::println);
+    }
+
+
+    private void getOrdersBetweenDates(){
+        LocalDate startDate = LocalDate.of(2026,9,13);
+        LocalDate endDate = LocalDate.of(2026,9,15);
+        List<Order> orderList =repo.fetchOrdersBetweenDates(startDate,endDate);
+
+        orderList.forEach(System.out::println);
+    }
 }
